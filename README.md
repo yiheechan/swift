0a5ba52ccda2fd6c30f2d6d044333f90bba56a19
yiheechan <lhc4002@gmail.com>
2017-03-11 14:56:19 +0900

    설명추가

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# swift
+# Swift 3.0
+## 스위프트 기본 문법
+
+###변수선언
+
+[바로가기](https://github.com/yiheechan/swift/SwiftStudy.playground/Pages/변수선언.xcplaygroundpage/Contents.swift)
+
+###자료형
+[바로가기](https://github.com/yiheechan/swift/SwiftStudy.playground/Pages/자료형.xcplaygroundpage/Contents.swift)
+###데이터타입
+[바로가기](https://github.com/yiheechan/swift/SwiftStudy.playground/Pages/데이터타입.xcplaygroundpage/Contents.swift)
+###배열
+[바로가기](https://github.com/yiheechan/swift/SwiftStudy.playground/Pages/배열.xcplaygroundpage/Contents.swift)
+.
+.
---
+++ SwiftTest/SwiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTest
+//
+//  Created by 이희찬 on 2017. 3. 10..
+//  Copyright © 2017년 이희찬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
